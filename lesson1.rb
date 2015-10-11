723292cc642dc767e83478be458fa9c42e7348d5
TupickiyV <vladislav.tupickiy@gmail.com>
2015-10-11 22:15:53 +0300

    Lesson 1 Vladislav Tupickiy

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,31 @@
 require 'date'
 class Lesson1
   def sum(val = 0)
+    sum = 0
+    val.to_s.scan(/[0-9]/) { |i| sum += i.to_i }
+    sum
     # TODO
   end
 
   def age(birthday)
+    if birthday
+      days = (Date.parse(Time.now.to_s) - Date.parse(birthday)).to_i
+      years = days / 365
+      hours = days * 24
+      minutes = hours * 60
+      seconds = minutes * 60
+      "Я живу #{years} года или #{days} дней
+          или #{hours} часов или #{minutes} минут или #{seconds} секунд"
+    else 'Invalid Date Format'
+    end
     # TODO
   end
 
   def name
+    name = gets.chomp
+    surname = gets.chomp
+    patronymits = gets.chomp
+    "Hello #{name} #{surname} #{patronymits}!"
     # TODO
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -6,58 +6,79 @@ class MyArray
   end
 
   def size
+    @array.size
     # TODO
   end
 
   def reverse
+    @array.reverse
     # TODO
   end
 
   def max
+    @array.max
     # TODO
   end
 
   def min
+    @array.min
     # TODO
   end
 
   def desc
+    @array.sort.reverse
     # TODO
   end
 
   def asc
+    @array.sort
     # TODO
   end
 
   def odd
+    @array.select { |i| i % 2 > 0 }
     # TODO
   end
 
   def multiple_to_three
+    @array.select { |i| i % 3 == 0 }
     # TODO
   end
 
   def uniq
+    @array.uniq
     # TODO
   end
 
   def devide_on_ten
+    @array.collect { |i| i / 10.to_f }
     # TODO
   end
 
   def chars
+    @array.collect { |i| ('a'...'z').to_a[i - 1].to_sym }
     # TODO
   end
 
   def switch
+    mx = @array.index(@array.max)
+    mn = @array.index(@array.min)
+    @array[mx], @array[mn] = @array[mn], @array[mx]
+    @array
     # TODO
   end
 
   def before_min
+    if @array.index(array.min) == 0
+      return []
+    else
+      @array.index(array.min - 1)
+    end
     # TODO
   end
 
   def three_smallest
+    @array.sort.take(3)
     # TODO
   end
 end
